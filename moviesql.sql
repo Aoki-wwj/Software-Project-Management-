64d1472aa5293877fa1a547f5da4e3e12ab8ee41
Aoki-wwj <125777594+Aoki-wwj@users.noreply.github.com>
2023-05-17 14:42:52 +0800

    Add files via upload

---
+++ moviesql.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.22 : Database - movie
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`movie` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `movie`;
+
+/*Table structure for table `ticket` */
+
+DROP TABLE IF EXISTS `ticket`;
+
+CREATE TABLE `ticket` (
+  `id` int NOT NULL,
+  `name` char(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `ticket` */
+
+insert  into `ticket`(`id`,`name`) values (2,'穿过寒冬拥抱你');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(10) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `nickname` char(10) DEFAULT NULL,
+  `phone` int DEFAULT NULL,
+  `sex` char(10) DEFAULT NULL,
+  PRIMARY KEY (`username`,`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`username`,`password`,`nickname`,`phone`,`sex`) values ('12','12','十九世纪',123456,'男'),('lisi','123','李四',123456,'男');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
